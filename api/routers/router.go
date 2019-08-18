7976e06a0d200f2e82b4e9c6beb767b7849d9923
liyongsheng <970806262@qq.com>
2019-08-19 01:17:53 +0800

    跨域

--- api/routers/router.go
+++ api/routers/router.go
@@ -10,11 +10,20 @@ package routers
 import (
 	"chat-room/api/controllers"
 	"chat-room/api/webscoket"
+	"github.com/astaxie/beego/plugins/cors"
 
 	"github.com/astaxie/beego"
 )
 
 func init() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type"},
+		AllowCredentials: true,
+	}))
+
 	// API
 	beego.AutoRouter(&controllers.ApiController{})
 
