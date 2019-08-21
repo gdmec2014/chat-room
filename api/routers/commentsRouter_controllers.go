a0eb824c8bb0437042160fc554e2f0249883a164
liyongsheng <970806262@qq.com>
2019-08-22 01:13:01 +0800

    1

---
+++ api/routers/commentsRouter_controllers.go
@@ -0,0 +1,50 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["chat-room/api/controllers:AuthController"] = append(beego.GlobalControllerRouter["chat-room/api/controllers:AuthController"],
+		beego.ControllerComments{
+			Method: "Login",
+			Router: `/login`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["chat-room/api/controllers:AuthController"] = append(beego.GlobalControllerRouter["chat-room/api/controllers:AuthController"],
+		beego.ControllerComments{
+			Method: "Register",
+			Router: `/register`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["chat-room/api/controllers:QiniuController"] = append(beego.GlobalControllerRouter["chat-room/api/controllers:QiniuController"],
+		beego.ControllerComments{
+			Method: "UptokenKey",
+			Router: `/uptoken_key`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["chat-room/api/controllers:QiniuController"] = append(beego.GlobalControllerRouter["chat-room/api/controllers:QiniuController"],
+		beego.ControllerComments{
+			Method: "UptokenWeb",
+			Router: `/uptoken_web`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["chat-room/api/controllers:UserController"] = append(beego.GlobalControllerRouter["chat-room/api/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "UpdateAvatar",
+			Router: `/update_avatar`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
